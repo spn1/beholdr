a8ab42f260f193a48af8e964e48220265b4cfe1d
Spencer <sn.newton92@gmail.com>
2023-11-11 18:33:54 +0000

    test

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -2,19 +2,22 @@ import { Card } from "@/components/home/Card";
 
 const Home = () => {
   return (
-    <main className="px-10 md:px-20 py-20 md:py-32 mx-auto h-screen bg-neutral-100">
+    <main className="p-10 md:py-20 mx-auto h-screen bg-neutral-100">
       <div className="sm:container h-full md:mx-auto">
         <div className="flex flex-col md:flex-row gap-10 lg:gap-20">
           <div className="md:basis-1/2 align-middle text-center">
-            <h1 className="text-6xl mb-4">
-              D&D 5e Rules Glossary
-            </h1>
+            <h1 className="text-6xl mb-4">D&D 5e Rules Glossary</h1>
             <p className="text-2xl">
-              Find the perfect creature to challenge your players, spell to cause destruction, or item to reward ingenuity
+              Find the perfect creature to challenge your players, spell to
+              cause destruction, or item to reward ingenuity
             </p>
           </div>
           <div className="md:basis-1/2 align-middle">
-            <img src="/images/tome.png" alt="beholder" className="aspect-square w-96 h-96 mx-auto drop-shadow-xl" />
+            <img
+              src="/images/tome.png"
+              alt="beholder"
+              className="aspect-square w-96 h-96 mx-auto drop-shadow-xl"
+            />
           </div>
         </div>
       </div>
